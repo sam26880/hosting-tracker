6d7ae2d1eb88092b268c769d245f493550b6ab15
Derek Fons <derek@farfromrest.com>
2012-10-17 16:53:36 -0500

    add default sort

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,11 +1,12 @@
 class Customer < ActiveRecord::Base
   attr_accessor :expiring_in_days
   attr_accessible :expiration_date, :name, :notes
+  default_scope order: :name
   validates :name, presence: true
   before_validation :update_expiration_date, if: :expiring_in_days
 
   def update_expiration_date
     self.expiration_date = Date.new + self.expiring_in_days.to_i.days
   end
-
+  
 end
