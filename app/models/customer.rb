51be0a11261209cb40cdef13808c72b718ce3955
Derek Fons <derek@farfromrest.com>
2012-10-17 16:34:24 -0500

    add validation for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,11 @@
 class Customer < ActiveRecord::Base
+  attr_accessor :expiring_in_days
   attr_accessible :expiration_date, :name, :notes
+  validates :name, presence: true
+  before_validation :update_expiration_date, if: :expiring_in_days
+
+  def update_expiration_date
+    self.expiration_date = Date.new + self.expiring_in_days.to_i.days
+  end
+
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,16 @@ describe Customer do
   it 'has a valid factory' do
     build(:customer).should be_valid
   end
+  it 'requires a name' do
+    build(:customer, name: nil).should_not be_valid
+  end
+  describe 'expiration date' do
+    it 'is a date object' do
+      create(:customer).expiration_date.should be_kind_of(Date)
+    end
+    it 'will update the date when given days instead of date' do
+      thirty_days_from_now = Date.new + 30.days
+      create(:customer, expiration_date: nil, expiring_in_days: 30).expiration_date.should eq(thirty_days_from_now)
+    end
+  end
 end
