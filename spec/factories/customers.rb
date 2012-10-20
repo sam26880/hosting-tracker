0bc7dbd624997abd65d201b7528046d9939eb303
Derek Fons <derek@farfromrest.com>
2012-10-20 16:06:35 -0500

    add invalid customer spec

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -5,5 +5,8 @@ FactoryGirl.define do
     name Faker::Name.first_name
     expiration_date "2012-10-17"
     notes Faker::Lorem.paragraph
+    factory :invalid_customer do
+      name nil
+    end
   end
 end
