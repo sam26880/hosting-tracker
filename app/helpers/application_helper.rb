fd98f3cc3f13390d50f06dc3c24c5be1d95c8da9
Derek Fons <derek@farfromrest.com>
2012-10-20 21:29:45 -0500

    add formatters for dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+  def format_date(date)
+    date.to_formatted_s(:long)
+  end
+
+  def format_month_day(date)
+    date.strftime("%b. %d")
+  end
+
+  def format_year(date)
+    date.year
+  end
+
+  def days_until(date, options = {})
+    options[:include_label] = true if options[:include_label].nil?
+    days = (date - Date.today).to_i
+    if options[:include_label]
+      "#{days} #{days_until_label date}"
+    else
+      "#{days}"
+    end
+  end
+
+  def days_until_label(date)
+    days = (date - Date.today).to_i
+    days == 1 ? 'day' : 'days'
+  end
 end
